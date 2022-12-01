b2e6b3137faa28c3568b9b3887e99acd2b37e179
Toby Wenman <tobywenman@gmail.com>
2022-12-01 18:02:03 +0000

    added syndrom calculation

--- task1_1.py
+++ task1_1.py
@@ -37,9 +37,26 @@ def encode(d,G):
     """Encode data to transmitted data"""
     return np.remainder(np.matmul(d,G),2)
 
+def calcSyndrome(H,c):
+    """Find sydrome for a recieved word"""
+    Ht = np.transpose(H)
+    return np.remainder(np.matmul(c,Ht),2)
+
+def generateSyndromes(H):
+    Ht = np.transpose(H)
+    n = np.shape(Ht)[0]
+    for i in range(n):
+        e = np.zeros(n)
+        e[i] = 1
+        print(np.matmul(e,Ht))
 
 # print(generatorMatrix(4))
 # print(parityCheckMatrix(generatorMatrix(4)))
 d = np.random.randint(0,2,11)
+G = generatorMatrix(4)
+H = parityCheckMatrix(G)
 print(d)
-print(encode(d,generatorMatrix(4)))
+print(encode(d,G))
+print(calcSyndrome(encode(d,G)+np.array([1]+14*[0]),H))
+
+#print(generateSyndromes(H))
