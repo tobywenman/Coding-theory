581c89d311750094c1706dafd408e7f628192014
Toby Wenman <tobywenman@gmail.com>
2022-12-09 00:01:28 +0000

    added main decoding functionality

--- task2_1.py
+++ task2_1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 
+#variable holding the structure of the state machine (codeword bits for 0 input,codeword bits for 1 input,(state for 0, state for 1))
 states = (((0,0),(1,1),(0,4)),
           ((1,1),(0,0),(0,4)),
           ((1,0),(0,1),(1,5)),
@@ -11,18 +12,60 @@ states = (((0,0),(1,1),(0,4)),
           ((0,0),(1,1),(3,7)))
 
 def nextState(states,prev,input):
+    """Return next state from state machine"""
     return states[prev][2][input]
 
 def getCodeword(states,prev,input):
+    """Return the bits added to the codeword"""
     return states[prev][input]
 
 def encode(states,d):
+    """Encode input data using states"""
     c = np.zeros(len(d)*2,dtype="int")
     state = 0
     for i in range(len(d)):
-        c[i*2:i*2+2] = getCodeword(states,state,d[i])
+        #Add bits to code word
+        c[i*2:i*2+2] = getCodeword(states,state,d[i]) 
+        #set next state
         state = nextState(states,state,d[i])
     return c
+
+def hammingDist(a,b):
+    """Calculate hamming distance"""
+    dist = 0
+    for i in range(len(a)):
+        #check if bits match
+        if a[i] != b[i]:
+            #count unmatched bits
+            dist += 1
+    return dist
+
+def decode(states,v):
+    #working list for the decoded path, path[decoded bit][state][weight or previous state]
+    path = np.array([[[math.inf,-1]]*8]*(len(v)//2))
+    path[0][0][0] = 0
+    for i in range(len(v)//2-1):
+        for j in range(8):
+            for k in [0,1]:
+                
+                weight = hammingDist(v[i*2:i*2+2],states[j][k])
+                nextState = states[j][2][k]
+                if weight+path[i][j][0] < path[i+1][nextState][0]:
+                    path[i+1][nextState][0] = weight+path[i][j][0]
+                    path[i+1][nextState][1] = k
+
+    #print(path[-10:])
+
+    decoded = np.zeros(len(path[:]),dtype=int)
+
+    for i in range(len(path[:])-1,0,-1):
+        minWeight = math.inf
+        for j in range(8):
+            if path[i][j][0] < minWeight:
+                minWeight = path[i][j][0]
+                decoded[i-1] = path[i][j][1]
+    return decoded
+
 
 k = 800
 termBits = 3
@@ -31,6 +74,7 @@ d = np.random.randint(0,2,k+termBits)
 
 d[-3:] = 0,0,0
 
-print(d)
-print()
 c = encode(states,d)
+
+decoded = decode(states,c)
+print(hammingDist(decoded,d))
